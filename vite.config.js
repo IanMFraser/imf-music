b4def56cae67f8a8a3b1d35047fc56af7368982b
IanMFraser <hunter_gracchus@hotmail.com>
2023-10-01 18:24:00 -0400

    migrates to vite

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+
+export default defineConfig(() => {
+    return {
+        build: {
+            outDir: 'build',
+        },
+        plugins: [react(), eslint()],
+        server: {
+            open: true,
+        },
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: './tests/setup.js',
+        },
+    }
+})
